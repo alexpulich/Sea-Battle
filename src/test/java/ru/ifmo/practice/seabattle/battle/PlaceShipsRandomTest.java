00d23e61c236a7f4efb1c4b415f48ec4bcec7527
Tarmahodik <hodik123@gmail.com>
2016-02-09 14:31:32 +0300

    Написал тест для бота В ходе тестирования обнаружились баги в игровой логике

--- src/test/java/ru/ifmo/practice/seabattle/battle/PlaceShipsRandomTest.java
+++ src/test/java/ru/ifmo/practice/seabattle/battle/PlaceShipsRandomTest.java
@@ -1,38 +1,12 @@
 package ru.ifmo.practice.seabattle.battle;
 
+import ru.ifmo.practice.seabattle.Console;
+
 public class PlaceShipsRandomTest {
     public static void main(String[] args) {
         Field field = new Field();
         field.placeShipsRandom();
 
-        Cell[][] fieldCells = field.getCurrentConditions();
-
-        for (Cell[] cells : fieldCells) {
-            for (Cell cell : cells) {
-                char symbol = 'o';
-
-                switch (cell) {
-                    case Void:
-                        symbol = 'v';
-                        break;
-
-                    case Ship:
-                        symbol = 's';
-                        break;
-
-                    case Miss:
-                        symbol = 'm';
-                        break;
-
-                    case Hit:
-                        symbol = 'h';
-                        break;
-                }
-
-                System.out.print(symbol);
-            }
-
-            System.out.print("\n");
-        }
+        Console.outputField(field);
     }
 }
---
+++ src/test/java/ru/ifmo/practice/seabattle/battle/bot/BotTest.java
@@ -0,0 +1,26 @@
+package ru.ifmo.practice.seabattle.battle.bot;
+
+import ru.ifmo.practice.seabattle.Console;
+import ru.ifmo.practice.seabattle.battle.Battle;
+import ru.ifmo.practice.seabattle.exceptions.IllegalNumberOfShipException;
+
+public class BotTest {
+    public static void main(String[] args) {
+
+        Bot bot1 = new Bot("Бот 1");
+        Bot bot2 = new Bot("Бот 2");
+
+        Console.outputField(bot1.getField(), bot1.getNickName());
+        Console.outputField(bot2.getField(), bot2.getNickName());
+
+        Battle battle = new Battle(bot1, bot2);
+        try {
+            battle.start();
+        } catch (IllegalNumberOfShipException e) {
+            e.printStackTrace();
+        }
+
+        Console.outputField(bot1.getField(), bot1.getNickName());
+        Console.outputField(bot2.getField(), bot2.getNickName());
+    }
+}
