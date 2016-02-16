ebab371d3743c4925d046261bcfc2753c3a08541
Александр Пашнин <panikun@yandex.ru>
2016-02-16 19:07:44 +0300

    Сменил максимальную длину email на 254 - стандарт RFC 5322

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE User (
   `id`            INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `user_nickname` VARCHAR(20)  NOT NULL UNIQUE,
-  `email`         VARCHAR(20)  NOT NULL UNIQUE,
+  `email`         VARCHAR(254)  NOT NULL UNIQUE,
   `games_count`   INT          NOT NULL DEFAULT '0',
   `wins_count`    INT          NOT NULL DEFAULT '0',
   `password`      VARCHAR(20)  NOT NULL,
